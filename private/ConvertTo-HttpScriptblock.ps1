8886c78a821bdb500fa45583f6ecee9bfed2f813
Tim Parkinson <tim+git@squidchunks.com>
2020-05-04 20:31:15 +0100

    basic async wrapper

---
+++ private/ConvertTo-HttpScriptblock.ps1
@@ -0,0 +1,69 @@
+function ConvertTo-HTTPScriptblock {
+    <#
+        .SYNOPSIS
+            Adds required HTTP functionality to an existing scriptblock.
+        .DESCRIPTION
+            Tops and tails a scriptblock with the required basic functionality to make it function over HTTP.
+        .PARAMETER ScriptBlock
+            The scriptblock to convert for HTTPS usage
+        .EXAMPLE
+            $new_scriptblock = ConvertTo-HTTPCScriptblock -ScriptBlock {Get-Process}
+        .OUTPUTS
+            System.Management.Automation.ScriptBlock
+    #>
+    [CmdletBinding()]
+
+    [OutputType([System.Management.Automation.ScriptBlock])]
+
+    param(
+        [Parameter(Mandatory=$true)]
+        [scriptblock]$ScriptBlock
+    )
+
+    begin {
+        $wrapper_scriptblock = {
+            param(
+                [System.Net.HttpListenerRequest]
+                $Request,
+                [System.Net.HttpListenerResponse]
+                $Response
+            )
+
+            try {
+                $output = Invoke-Command -ScriptBlock {
+                    param($request)
+
+                    REPLACEWITHSCRIPTBLOCK
+                } -ArgumentList $request
+            } catch {
+                $status_code = [System.Net.HttpStatusCode]::InternalServerError
+                $content = $_.ToString()
+                $content_type = ''
+            }
+
+            if (-not $status_code) {
+                $status_code = [System.Net.HttpStatusCode]::OK
+            }
+
+            if ($output) {
+                $json_output = $output |
+                    ConvertTo-Json -Depth 100
+                $content_type = 'text/json'
+            }
+
+            $buffer = [Text.Encoding]::UTF8.GetBytes($json_output)
+            
+            $Response.StatusCode = $status_code
+            $Response.ContentType = $content_type
+            $Response.ContentLength64 = $buffer.Length
+            $Response.OutputStream.Write($buffer, 0, $buffer.Length)
+
+            $Response.Close()
+        }
+    }
+
+    process {
+        [scriptblock]::Create($wrapper_scriptblock.ToString().Replace('REPLACEWITHSCRIPTBLOCK',$Scriptblock.ToString()))
+    }
+
+}
