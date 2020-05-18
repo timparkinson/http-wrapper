5990045334dceb770def2b3f0ce1c2849c582087
Tim Parkinson <tim+git@squidchunks.com>
2020-05-18 15:03:37 +0100

    try installing pester for ubuntu

--- tasks.ps1
+++ tasks.ps1
@@ -7,6 +7,13 @@ param(
 
 switch ($Task) {
     'Test' {
+        if (-not (Get-Module -Name 'Pester')) {
+            Get-PackageProvider -Name Nuget -ForceBootstrap |
+                Out-Null
+            Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+            Install-Module -Name Pester -Repository PSGAllery -Scope CurrentUser -Force
+        }
+
         Import-Module Pester
         Invoke-Pester
     }
