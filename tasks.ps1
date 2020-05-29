6029457c7891e8cb821c2e1ab900769bb47aeb50
Tim Parkinson <tim+git@squidchunks.com>
2020-05-29 13:45:05 +0100

    PSGallery TLS workaround

--- tasks.ps1
+++ tasks.ps1
@@ -13,6 +13,8 @@ param(
 )
 
 if ($Bootstrap) {
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 
+    Install-Module PowerShellGet -RequiredVersion 2.2.4 -SkipPublisherCheck
     Get-PackageProvider -Name Nuget -ForceBootstrap |
         Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
