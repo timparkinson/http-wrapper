451cab3b1e62a588146c619a52c8fe7f15d8a52d
Tim Parkinson <tim+git@squidchunks.com>
2020-06-21 14:33:29 +0100

    refactor tests to hopefully break less

--- tests/server.Tests.ps1
+++ tests/server.Tests.ps1
@@ -4,50 +4,65 @@ BeforeAll {
     $module_path = Split-Path -Parent -Path $here
     Import-Module "$module_path/http-wrapper.psd1"
     
-    $port_basic = 8080
-    $port_sleep = 8081
-    $port_module = 8082
-    $port_shared = 8083
-    $port_error = 8084
-    $port_request_response = 8085
+    $port = 8080
 
-    $server_basic = New-HttpWrapper -Scriptblock {@{'hello'='world'}} -Port $port_basic
-    $server_sleep = New-HttpWrapper -Scriptblock {Start-Sleep -Seconds 10; @{'hello'='world'}} -Port $port_sleep -NumListenThread 10
-    $server_module = New-HttpWrapper -Scriptblock {(Get-Module).Name} -Port $port_module -Module 'Microsoft.Powershell.Archive'
-    $server_shared = New-HttpWrapper -Scriptblock {@{'hello'=$SharedData.hello}} -Port $port_shared -BootstrapScriptblock {$SharedData.bootstrap = 'banana'}
-    $server_error = New-HttpWrapper -Scriptblock {Write-Error -Message 'oh noes' -ErrorAction Stop} -Port $port_error
-    $server_request_response = New-HttpWrapper -Scriptblock {@{'request' = $Request; 'response' = $Response}} -Port $port_request_response
+    $scriptblock = {
+        if ($Request.Url.AbsolutePath -match '\/(?<route>\w+)') {
+            $route = $matches.route
+        } else {
+            Write-Error -Message "No route found" -ErrorAction Stop
+        }
+        
+        switch ($route) {
+            'basic' {
+                @{'hello'='world'}
+            }
 
-    Start-HttpWrapper -HttpWrapper $server_basic
-    Start-HttpWrapper -HttpWrapper $server_sleep
-    Start-HttpWrapper -HttpWrapper $server_module
-    Start-HttpWrapper -HttpWrapper $server_shared
-    Start-HttpWrapper -HttpWrapper $server_error
-    Start-HttpWrapper -HttpWrapper $server_request_response
+            'sleep' {
+                Start-Sleep -Seconds 10; @{'hello'='world'}
+            }
+
+            'module' {
+                (Get-Module).Name
+            }
+
+            'shared' {
+                @{'hello'=$SharedData.hello}
+            }
+
+            'error' {
+                Write-Error -Message 'oh noes' -ErrorAction Stop
+            }
+
+            'requestresponse' {
+                @{'request' = $Request; 'response' = $Response}
+            }
+        }
+       
+    }
+    
+    $server = New-HttpWrapper -Scriptblock $scriptblock -Port $port -NumListenThread 10 -Module 'Microsoft.Powershell.Archive' -BootstrapScriptblock {$SharedData.bootstrap = 'banana'}
+    
+    Start-HttpWrapper -HttpWrapper $server
 }
 Describe "Server" {
     
     It "creates a server" {
-        $server_basic | Should -Not -BeNullOrEmpty
+        $server | Should -Not -BeNullOrEmpty
     }
 
     It "should start" {
-        $server_basic.Listener.IsListening | Should -Be $true
-        $server_sleep.Listener.IsListening | Should -Be $true
-        $server_module.Listener.IsListening | Should -Be $true
-        $server_shared.Listener.IsListening | Should -Be $true
-        $server_error.Listener.IsListening | Should -Be $true
-        $server_request_response.Listener.IsListening | Should -Be $true
+        $server.Listener.IsListening | Should -Be $true
     }
 
     It "gets a result" {
-        $result = Invoke-RestMethod -Uri "http://localhost:$port_basic/"
+        $result = Invoke-RestMethod -Uri "http://localhost:$port/basic"
 
         $result.hello | Should -Be 'world'
     }
 
     It "gets a second result" {
-        $result = Invoke-RestMethod -Uri "http://localhost:$port_basic/"
+        $result = Invoke-RestMethod -Uri "http://localhost:$port/basic"
 
         $result.hello | Should -Be 'world'
     }
@@ -58,7 +73,7 @@ Describe "Server" {
         $timer.Start()
         
         $results = 1..6 | ForEach-Object -Parallel {
-            Start-Job -ScriptBlock {Invoke-RestMethod -Uri "http://localhost:8081/"}
+            Start-Job -ScriptBlock {Invoke-RestMethod -Uri "http://localhost:8081/sleep"}
            #Invoke-RestMethod -Uri "http://localhost:$($using:port_sleep)/"
         }
         $results = Get-Job | Receive-Job -Wait
@@ -70,50 +85,40 @@ Describe "Server" {
     }
 
     It "loads modules" {
-        $result = Invoke-RestMethod -Uri "http://localhost:$port_module/"
+        $result = Invoke-RestMethod -Uri "http://localhost:$port/module"
 
         $result | Should -Contain 'Microsoft.Powershell.Archive'
     }
 
     It "allows SharedData to be passed" {
-        $server_shared.SharedData.hello = 'worldshared'
+        $server.SharedData.hello = 'worldshared'
 
-        $result = Invoke-RestMethod -Uri "http://localhost:$port_shared/"
+        $result = Invoke-RestMethod -Uri "http://localhost:$port/shared"
 
         $result.hello | Should -Be 'worldshared'
         
     }
 
     It "runs a setup script block" {
-        $server_shared.SharedData.bootstrap | Should -Be 'banana'
+        $server.SharedData.bootstrap | Should -Be 'banana'
     }
 
     It "accesses request/response" {
-        $result = Invoke-RestMethod -Uri "http://localhost:$port_request_response/"
+        $result = Invoke-RestMethod -Uri "http://localhost:$port/requestresponse"
 
         $result.request | Should -Not -BeNullOrEmpty
         $result.response | Should -Not -BeNullOrEmpty
     }
 
     It "handles errors" {
-        {Invoke-RestMethod -Uri "http://localhost:$port_error/"} | 
+        {Invoke-RestMethod -Uri "http://localhost:$port/error"} | 
             Should -Throw
     }
 
     It "stops" {
-        Stop-HttpWrapper -HttpWrapper $server_basic
-        Stop-HttpWrapper -HttpWrapper $server_sleep
-        Stop-HttpWrapper -HttpWrapper $server_module
-        Stop-HttpWrapper -HttpWrapper $server_shared
-        Stop-HttpWrapper -HttpWrapper $server_error
-        Stop-HttpWrapper -HttpWrapper $server_request_response
-
-        $server_basic.Listener.IsListening | Should -Be $false
-        $server_sleep.Listener.IsListening | Should -Be $false
-        $server_module.Listener.IsListening | Should -Be $false
-        $server_shared.Listener.IsListening | Should -Be $false
-        $server_error.Listener.IsListening | Should -Be $false
-        $server_request_response.Listener.IsListening | Should -Be $false
+        Stop-HttpWrapper -HttpWrapper $server
+    
+        $server.Listener.IsListening | Should -Be $false
     }
 
 }
